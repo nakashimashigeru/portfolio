79337ae605f4756c89fbeceab364e7661c3212bb
nakashimashigeru <nakashimashigeru1951.9.17@gmail.com>
2024-03-29 20:13:56 +0900

    環境変数使用（firebaseConfig）

--- app/libs/firebase/config.tsx
+++ app/libs/firebase/config.tsx
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const provider = new GoogleAuthProvider();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3P1ANq7Ukl3woyc3eWXrXINcVhNnIys8",
-  authDomain: "project-7508986809828098372.firebaseapp.com",
-  projectId: "project-7508986809828098372",
-  storageBucket: "project-7508986809828098372.appspot.com",
-  messagingSenderId: "304501196966",
-  appId: "1:304501196966:web:dd5e3129177395a2730c67"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
 };
 
 const app = firebase.initializeApp(firebaseConfig);
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true }
+  output: "export",
+  images: { unoptimized: true },
+  env: {
+    apiKey: "AIzaSyB3P1ANq7Ukl3woyc3eWXrXINcVhNnIys8",
+    authDomain: "project-7508986809828098372.firebaseapp.com",
+    projectId: "project-7508986809828098372",
+    storageBucket: "project-7508986809828098372.appspot.com",
+    messagingSenderId: "304501196966",
+    appId: "1:304501196966:web:dd5e3129177395a2730c67"
+  },
 }
 
 module.exports = nextConfig
